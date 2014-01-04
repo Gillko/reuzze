428c334e0adebb2ecb062a61d2ed9304fa9e8158
Gilles Vanpeteghem <gilles_vanpeteghem@hotmail.com>
2014-01-04 13:49:30 +0100

    boolean to integer

--- src/Reuzze/ReuzzeBundle/Entity/Categories.php
+++ src/Reuzze/ReuzzeBundle/Entity/Categories.php
@@ -13,9 +13,9 @@ use Doctrine\ORM\Mapping as ORM;
 class Categories
 {
     /**
-     * @var boolean
+     * @var integer
      *
-     * @ORM\Column(name="category_id", type="boolean")
+     * @ORM\Column(name="category_id", type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="IDENTITY")
      */
@@ -71,7 +71,7 @@ class Categories
     /**
      * Get categoryId
      *
-     * @return boolean 
+     * @return integer
      */
     public function getCategoryId()
     {
