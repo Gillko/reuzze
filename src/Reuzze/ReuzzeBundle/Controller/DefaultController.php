2451853efc8786eb38c96aa519b9d2e2e88242d3
Gilles Vanpeteghem <gilles_vanpeteghem@hotmail.com>
2014-01-04 16:59:53 +0100

    css - show on homepage entities - create entity page changes

--- src/Reuzze/ReuzzeBundle/Controller/DefaultController.php
+++ src/Reuzze/ReuzzeBundle/Controller/DefaultController.php
@@ -4,20 +4,20 @@ namespace Reuzze\ReuzzeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
-
 class DefaultController extends Controller
 {
-    public function homeAction()//$name)
+    public function homeAction()
     {
-        //return $this->render('ReuzzeReuzzeBundle:Default:index.html.twig', array('name' => $name));
-
         $entityManager = $this->getDoctrine()->getManager();
         $categories = $entityManager->getRepository('ReuzzeReuzzeBundle:Categories')
             ->findAll();
 
+        $entities = $entityManager->getRepository('ReuzzeReuzzeBundle:Entities')
+            ->findAll();
+
         return $this->render('ReuzzeReuzzeBundle:Default:home.html.twig', array(
             'categories' => $categories,
+            'entities' => $entities,
         ));
     }
 }
